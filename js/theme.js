753e8f52181f75f3592c4148408d782414be8c9b
Febby Gunawan <gunawan.febby@gmail.com>
2014-12-11 01:59:49 +0800

    updated vids markup and add relevant js to make it work

--- js/theme.js
+++ js/theme.js
@@ -1185,9 +1185,38 @@
 
 		$(window).load(function(){
 
-			$.stickyMenu();
-			smoothScroll.init();
-			$.sideMenu();
+			// $.stickyMenu();
+			 $(function() {
+                $("ul#rp_playlist").responsiveplaylist({
+                    autoPlay: false,
+                    autoPlayOnLoad: false,
+                    allowFullScreen: true,
+                    deepLinks: true,
+                    onChange: function(){},
+                    start: 1,
+                    youtube: {
+                        autohide: '2',
+                        rel: '1',
+                        theme: 'dark',
+                        color: 'white',
+                        showinfo: '1',
+                        vq: 'medium'
+                    },
+                    vimeo: {
+                        title: '1',
+                        byline: '1',
+                        portrait: '1',
+                        color: '00adef'
+                    },
+                    // youtubeUsername: 'username',
+                    // vimeoUsername: 'username',
+                    // youtubePlaylist: 'XXXXXXXXXXXXXXXXXX',
+                    // vimeoAlbum: 'XXXXXXX',
+                    holderId: 'rp_video',
+                    secure: 'auto'
+                });
+            });
+			// $.sideMenu();
 			$.responsiveMenu();
 			$.backToTop(1000);
 			$.correctImagesPosition();
